d695f95380ad00dd12600fd8b9365dbf2ce95f0d
Bo Hu <b.hu@yale.edu>
2018-03-05 20:32:42 -0500

    add sessionID

--- src/main/java/PlateRecognizer.java
+++ src/main/java/PlateRecognizer.java
@@ -19,6 +19,7 @@ import edgeOffloading.OffloadingOuterClass.OffloadingReply;
 public class PlateRecognizer {
 	static ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 	public static void main(String[] args) {
+		String sessionID = args[0];
 		Alpr alpr = new Alpr("us", "openalpr.conf", "runtime_data");
 		System.out.println("Start a new process!");
 		// Set top N candidates returned to 20
@@ -30,7 +31,7 @@ public class PlateRecognizer {
 				alpr.recognize("receive_1.jpg");
 				long end = System.currentTimeMillis();
 				double currentRate = 50370.0/(end-begin);
-				updateInfo(currentRate);
+				updateInfo(currentRate, sessionID);
 				if(end - SessionBegin >= 800 * 1000) {
 					System.out.println("Finish this session!");
 					break;
@@ -41,12 +42,12 @@ public class PlateRecognizer {
 			}
 		}
 	}
-	private static void updateInfo(double rate) {
+	private static void updateInfo(double rate, String sessionID) {
 		ManagedChannel mChannel;
 		mChannel = ManagedChannelBuilder.forAddress("34.218.103.6", 50050).usePlaintext(true).build();
 		OffloadingGrpc.OffloadingBlockingStub stub = OffloadingGrpc.newBlockingStub(mChannel);
 		String hostIP = System.getenv("HOSTIP");
-		OffloadingRequest message = OffloadingRequest.newBuilder().setMessage(hostIP + ":" + "plate" + ":" + Double.toString(rate)).build();
+		OffloadingRequest message = OffloadingRequest.newBuilder().setMessage(hostIP + ":" + sessionID + ":" + "plate" + ":" + Double.toString(rate)).build();
 		OffloadingReply reply = stub.startService(message);
 	}
 }
